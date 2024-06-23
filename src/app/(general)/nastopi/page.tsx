f741460dcf2258965bcf761ad6017c49b0ad9317
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-06-23 17:51:52 +0200

    added odmevi intiial

--- src/app/(general)/nastopi/page.tsx
+++ src/app/(general)/nastopi/page.tsx
@@ -1,8 +1,237 @@
 
 export default function Nastopi() {
   return (
-    <main>
-        <h1>Nastopi</h1>
+    <main className="bg-white text-black px-4 py-8 md:px-16">
+      <h1 className="text-2xl font-semibold pb-4">Gledališče</h1>
+      <h2 className="text-xl text-neutral-700 py-4"><span className="font-semibold">MOR</span> (Mladi oder Ruše) je CEZAM (Center za mlade Ruše) zasnoval leta 2014 z režiserko Tanjo Lužar.</h2>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2015</h3>
+        <div>
+          <p>s predstavo <em className="font-semibold">Vse ob pravem času</em> (David Ives) uvrstitev na regijsko tekmovanje Linhartovega srečanja.</p>
+          <p>Režija: Tanja Lužar.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2016</h3>
+        <div>
+          <p>Performans (recital) Glazerjeve poezije na Glazerjevih dnevih v Domu kulture Ruše, marec 2016;</p>
+          <p>Režija: Tanja Lužar</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2017</h3>
+        <div>
+          <p>Glasbeno-lutkovno gledališka predstava <em className="font-semibold">Mali princ</em>, Dom kulture Ruše</p>
+          <p>Režija: Cvetka Bevc</p>
+          <p>Uglasbene pesmi: Peter Andrej</p>
+          <p>Lastna produkcija Cezama, društva Glazerjeva domačija in Kluba KU KU v sodelovanju z JSKD</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2018</h3>
+        <div>
+          <p>Glasbeno-lutkovno gledališka predstava <em className="font-semibold">Mali princ</em>, Gostovanje v gledališču v Osijeku, maj 2018</p>
+          <p>Režija: Cvetka Bevc</p>
+          <p>Uglasbene pesmi: Peter Andrej</p>
+        </div>
+      </section>
+
+      <h2 className="text-xl text-neutral-700 py-4 font-semibold">Kamišibaj</h2>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2018</h3>
+        <div>
+          <p><em className="font-semibold">Rege ali žabje frke</em>; knjižnica Janka Glazerja Ruše, 7. 12. 2018</p>
+          <p>Napisal: Peter Andrej</p>
+          <p>Ilustrirala: Kaja Lukač</p>
+          <p>Za kamišibaj priredila in ga izvedla: Mojca Andrej</p>
+          <p>Pojeta: Peter Andrej in Barbara Gabrielle</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2019</h3>
+        <div>
+          <p><em className="font-semibold">Agica, mala čarovnica</em>; knjižnica Janka Glazerja Ruše, 11. 12. 2019</p>
+          <p>Napisala: Mojca Andrej</p>
+          <p>Ilustrirala: Darka Erdelji</p>
+          <p>Pesmi napisal, uglasbil in zapel: Peter Andrej</p>
+          <p>Za kamišibaj priredila in ga izvedla: Mojca Andrej</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2020</h3>
+        <div>
+          <p>Glasbeni kamišibaj <em className="font-semibold">Agica, mala čarovnica</em>; Mariborska knjižnica, Knjižnica Nova vas, februar 2020.</p>
+          <p>Predstavitev avtorske sodobne pravljice <em className="font-semibold">Agica, mala čarovnica</em>. Glasbeni kamišibaj ob podpori glasbe (uglasbene pesmi iz pravljice) kantavtorja Petra Andreja in gledališke igralke Alenke Cilenšek. Oder knjižnega sejma Glavni trg Maribor – Slovenski dnevi knjige (organizator MKC); 21. 6. 2020</p>
+        </div>
+      </section>
+
+      <h2 className="text-xl text-neutral-700 py-4 font-semibold">Magnetno Gledališče</h2>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2022</h3>
+        <div>
+          <p>Premierna predstava z magnetnimi lutkami <em className="font-semibold">Agica, mala čarovnica</em>; gostovanje v Avstriji v Gradcu, Pavlova hiša, Laafeld, junij 2022</p>
+          <p>Nastopali: Mojca Andrej, Peter Andrej in Alenka Cilenšek.</p>
+          <p>Povabilo mag. Susanne Weitlaner (Kulturno društvo Člen 7 za avstrijsko Štajersko) in Tatjane Vučajnk, predstava za otroke, ki govorijo in se učijo slovenščino.</p>
+          <p>Prenovljeno podobo Agice, male čarovnice sta sooblikovala Darka Erdelji (oblikovanje lutk in likovna podoba) in Urban Saletinger (izdelava scene).</p>
+          <p>12. Glazerjevi dnevi: muzikal za otroke, <em className="font-semibold">Agica mala čarovnica</em>; Gostitelj: Zavod RAST Ruše, Občina Ruše, december 2022</p>
+          <p>Nastopali: Mojca Andrej, Peter Andrej in Alenka Cilenšek</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2023</h3>
+        <div>
+          <p>12. Pohorska pravljica: predstava (muzikal) <em className="font-semibold">Jezernik, povodni mož iz Črnega jezera</em>. V muzikal sta ga v svojo mini magnetno gledališče ujela Mojca Andrej in Peter Andrej.</p>
+          <p>Soorganizatorji: Letni oder Ruše, Zavod Rast Ruše, Glazerjeva domačija / Glazerjevi dnevi, Klub KU KU in Občina Ruše.</p>
+        </div>
+      </section>
+
+      <h1 className="text-2xl text-black py-4 font-semibold">Nastopi in Predstavitve</h1>
+
+      <h2 className="text-xl text-neutral-700 py-4 font-semibold">Doma</h2>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2015</h3>
+        <div>
+          <p>Predstavitev pesniške zbirke <em className="font-semibold">Dež v gugalnici</em>; Literarna hiša Maribor, julij 2015. Z avtorico se je pogovarjal Marjan Pungartnik.</p>
+          <p>Predstavitev poezije za otroke <em className="font-semibold">Rastem do tebe</em>, september 2015. Organizator: Kulturno – gledališko društvo Reciklaža, Slovenske Gorice.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2016</h3>
+        <div>
+          <p>Predstavitev pesniške zbirke <em className="font-semibold">Dež v gugalnici</em>, Klub Daktari v Ljubljani, november 2016. Pogovor je vodila Cvetka Bevc, Peter Andrej je uglasbil in zapel nekaj pesmi iz te pesniške zbirke.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2020</h3>
+        <div>
+          <p>Branje poezije na Slovenskih dnevih knjige, Grajski trg Maribor, junij 2020.</p> 
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2021</h3>
+        <div>
+          <p>Branje poezije na literarnem maratonu z Združenimi državami poezije in predstavitvijo knjige <em className="font-semibold">Janko Glazer v prevodu</em>; 19. 6. 2021, Grajski trg Maribor. V soorganizaciji z MKC Maribor in Klubom kulturnih ustvarjalcev KU KU.</p>
+          <p>Predstavitev pesniške zbirke <em className="font-semibold">Ostanek umrle zvezde</em>; Slovenski dnevi knjige v Mariboru, Minoriti, 20. junij 2021. Literarno branje Literinih avtorjev in avtorice. Nastopili so Vasja Jager, Mojca Andrej, Bojan Sedmak in Tomo Podstenšek. Povezoval je Orlando Uršič. Prireditev je organiziral MKC Maribor v sodelovanju z Založbo Litera. Izvedba: MKC Maribor in KGB v sodelovanju z LGM.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2022</h3>
+        <div>
+          <p>Prisotnost na predstavitvi slikanice <em className="font-semibold">Agica, mala čarovnica</em>, ki jo je študentka predšolske vzgoje Eva Tomažin predstavila in analizirala pri predmetu Mladinska književnost, mentorica dr. Milena Mileva Blažić; Pedagoška fakulteta v Ljubljani, oktober 2022.</p>
+          <p>Prva predstavitev romana <em className="font-semibold">Kavč učiteljice Veronike</em>; na OŠ Prežihovega Voranca Maribor v soorganizaciji založbe Litera in šole, 29. november 2022. Z avtorico se je pogovarjala urednica založbe Gabriela Babnik Ouattara.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0" >2023</h3>
+        <div>
+          <p>Predstavitev romana <em className="font-semibold">Kavč učiteljice Veronike</em>; Konjeniški park Starošince - Nazaj na konja, 3. 10. 2023. Z avtorico se je pogovarjala Metka Demšar Goljevšček.</p>
+          <p>Predstavitev romana <em className="font-semibold">Kavč učiteljice Veronike</em>; Antika knjigarna Celje, 7. 11. 2023. Nekatere Veronikine pesmi iz <em className="font-semibold">Blagajne misli</em> je odigral Peter Andrej. Z avtorico se je pogovarjal Matej Krajnc.</p>
+          <p>Center za poezijo Tomaža Šalamuna, Ljubljana; 9. 11. 2023. Avtorico romana <em className="font-semibold">Kavč učiteljice Veronike</em> je poglobljeno predstavila Barbara Rigler. Tatjana Vidmar in avtorica sta prebrali odlomke iz knjige. Nekatere Veronikine pesmi iz <em className="font-semibold">Blagajne misli</em> je odigral Peter Andrej.</p>
+          <p>13. Glazerjevi dnevi na gostovanju, 15. 11. 2023, v Literarni hiši Maribor: promocija knjige Mojca Andrej, <em className="font-semibold">Kavč učiteljice Veronike</em>. Nekatere Veronikine pesmi iz <em className="font-semibold">Blagajne misli</em> je odigral Peter Andrej. Z avtorico se je pogovarjal Marjan Pungartnik.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0" >2024</h3>
+        <div>
+          <p>27. festival Slovenski dnevi knjige Na preži v Mariboru, 22. 5. 2024, Grajski trg Maribor.</p>
+          <p><em className="font-semibold">Pijani čoln v Delti jezika, na Tnalu pesmi pa Prehodi: Literarni performans.</em> Nastopali: Mojca Andrej, Peter Andrej, Bojan Tomažič in Bojan Sedmak.</p>
+          <p>Knjižni sejem Na preži v Murski Soboti, 15. 6. 2024.</p>
+          <p><em className="font-semibold">Pijani čoln v Delti jezika, na Tnalu pesmi pa Prehodi: Literarni performans.</em> Nastopali: Mojca Andrej, Peter Andrej, Bojan Tomažič in Bojan Sedmak.</p>
+        </div>
+      </section>
+
+      <h3 className="text-lg text-neutral-800 py-4 font-semibold">Knjižni Sejem</h3>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2019</h3>
+        <div>
+          <p>Slikanica <em className="font-semibold">Agica, mala čarovnica</em> na 35. slovenskem knjižnem sejmu, v paviljonu založbe Litera, v Cankarjevem domu v Ljubljani.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2022</h3>
+        <div>
+          <p>Roman <em className="font-semibold">Kavč učiteljice Veronike</em> na 38. slovenskem knjižnem sejmu; na stojnici založbe Litera, na Gospodarskem razstavišču v Ljubljani.</p>
+        </div>
+      </section>
+
+      <h2 className="text-xl text-neutral-700 pt-4 pb-2 font-semibold">Tujina</h2>
+      <h4 className="text-lg text-neutral-500">Hrvaška, Bosna in Hercegovina, Srbija, Severna Makedonija, Bolgarija, Avstrija, Palma de Mallorca, Maroko.</h4>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <div className="flex flex-col items-center">
+          <h3 className="text-sm font-light mt-1 md:mt-0">2012</h3>
+          <h3 className="text-sm font-light mt-1 md:mt-0">–</h3>
+          <h3 className="text-sm font-light mt-1 md:mt-0">2019</h3>
+        </div>
+        <div>
+          <p>Poetski festival Neretvanske vedrine; Počitelj, Mostar, Sarajevo, BIH. Literarna branja</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2017</h3>
+        <div>
+          <p>Gostovanje na festivalu Impuls – poezija u kafiću; Varaždin, 25. 1. 2017. Organizator: Ritam misli. Moderatorja: Ljubica Ribić, Milan Novak</p>
+          <p>Branje poezije v Beogradu, v Matici iseljenika Srbije, 16. 11. 2017. Nastop s Petrom Andrejem, Jernejem Mažgonom in Grgo Olahom iz Madžarske. Gostitelj Kuča Arte, Miodrag Jakšić.</p>
+          <p>Glazerjevi dnevi v Pančevu, Narodni muzej Pančevo, Društvo Slovencev južnega Banata Logarska dolina, gostitelj Josip Weber, 17. 11. 2017</p>
+          <p>44. Karamanova srečanja, Severna Makedonija, Radoviš, 1. in 2. 12. 2017, branje poezije, tudi program za otroke.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2018</h3>
+        <div>
+          <p>Poetics international festival june in Sofia, Bolgarija, 16. – 17. junij 2018.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2019</h3>
+        <div>
+          <p>Festival SOFAFEST; branje poezije v Palmi de Mallorci, maj 2019.</p>
+          <p>100 Илјади поети за промена, Struška srečanja; Strumica, Severna Makedonija; literarni nastopi, september 2019.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2022</h3>
+        <div>
+          <p>Majska srečanja slovanskih umetnikov; Burgas, Bolgarija, 25. – 29. 5. 2022. Gostitelj: ekipa СВЯТО СЛОВО. Združene Države Poezije so v Burgasu predstavile nekaj slovenskih knjig, ki so doživele tudi prevod v bolgarski jezik: <em className="font-semibold">Janko Glazer v prevodu</em> (Klub KU KU, Cezam 2020), <em className="font-semibold">Mojca Andrej: Ostanek umrle zvezde</em> (Litera Maribor, 2020), <em className="font-semibold">Peter Andrej: Skoz zvočni zid</em> (Založba Litera, 2019).</p>
+          <p>Takt festival; Novi Sad: 20. KANTFEST je svoj International nadaljeval v Novem Sadu. Nastop: Bojan Sedmak, Mojca Andrej, Neaboinula (Bojan Tomažič) in Peter Andrej.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2023</h3>
+        <div>
+          <p>Branje na predstavitvi projekta <em className="font-semibold">VSI SMO MI ENO/SVI SMO MI JEDNO</em> Muzej suvremene umetnosti Zagreb, 18. maj 2023.</p>
+        </div>
+      </section>
+
+      <section className="text-neutral-900 py-2 flex space-x-4 items-start md:items-center md:px-4 px-2">
+        <h3 className="text-sm font-light mt-1 md:mt-0">2024</h3>
+        <div>
+            <p>Festival pesnikov petih kontinentov - <em className="font-semibold">POETS FROM FIVE CONTINENTS</em> – Maroko (Laayoune, Safi, Tangiers, Meknes in Marakeš), 19. – 28. 4. 2024. Organizator: Al Kalima Institution Of Culture And Arts In Safi, The Forum Of The Sahraoui Woman, Development And Democracy, Laayoune, Morocco.</p>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
