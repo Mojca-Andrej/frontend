cdb906ac8555092513eec2638563cb1a7ce25888
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-06-24 11:26:07 +0200

    odmevi initial

---
+++ src/app/(general)/odmevi/data/data.ts
@@ -0,0 +1,70 @@
+export enum TipiOdmevov {
+    veronika,
+    ars,
+    razno,
+    zvezda
+}
+
+
+
+export interface Odmevi {
+    title:string, 
+    src:string,
+    type: TipiOdmevov
+}
+
+
+export const odmevi: Odmevi[] = [
+    {
+        title: "O pesniški zbirki Ostanek umrle zvezde, Maribor24",
+        src: "https://maribor24.si/kultura/mojca-andrej-izdala-svojo-tretjo-pesnisko-zbirko",
+        type: TipiOdmevov.zvezda
+    },
+    {
+        title: "Radio Maribor, novinarka Brigita Mohorič, januar 2023, spletni zapis",
+        src:  `https://www.rtvslo.si/radio-maribor/mojca-andrej-zgodba-uciteljice
+        -veronike-je-pripoved-o-danasnji-druzbi-o-mnogih-med-nami/655484`,
+        type: TipiOdmevov.veronika
+    },
+    {
+        title: `Predstavitev romana v Rušah, 15. 12. 2022, kratki video`,
+        src: `https://www.youtube.com/watch?v=uT__ARDYqTc`,
+        type: TipiOdmevov.veronika
+    },
+    {
+        title: `Intervju na radiu Maribor (od 7.55 naprej) z Brigito Mohorič`,
+        src: `https://www.rtvslo.si/radio/podkasti/kultura-zdravi-umetnost-
+        lajsa/155779478/174926726`,
+        type: TipiOdmevov.veronika
+    },
+    {
+        title: `Podcast Zorni kot z Natašo Rižnar, januar 2023`,
+        src: `https://www.rtvslo.si/radio-maribor/podcast-zorni-kot-18-otroci-mislijo-da-so-vsi-odgovori-na-googlu-oziroma-le-klik-stran/656679?fbclid=IwAR1QVIaVSz3A1CpwgMwskysCjZx6qctmmt3j08TJn2Fuuvn5rT0WoxKD-OQ`,
+        type: TipiOdmevov.veronika
+    },
+    {
+        title: `intervju`,
+        src: `https://www.arslitera.org/2023/02/07/pescena-ura-ne-tiktaka-neslisno-polzi-pogovor-z-mojco-andrej/`,
+        type: TipiOdmevov.ars
+    },
+    {
+        title: `odlomki`,
+        src: `https://www.arslitera.org/2023/02/07/kavc-uciteljice-veronike-odlomki/`,
+        type: TipiOdmevov.ars
+    },
+    {
+        title: "Radio Slovenske gorice – Pod zvezdami",
+        src: "https://www.rsg.si/2023/03/02/pod-zvezdami-mojca-andrej/",
+        type: TipiOdmevov.razno
+    },
+    {
+        title: "Matej Krajnc v Kulturno-medijskem centru Slovenije",
+        src: "https://homocumolat.com/2023/10/09/mojca-andrej-kavc-uciteljice-veronike/",
+        type: TipiOdmevov.razno
+    },
+    {
+        title: "Primorske novice, 21. 6. 2024",
+        src: "https://primorske.svet24.si/kultura/knjizna-polica/do-sijaja-zlosceno-ogledalo",
+        type: TipiOdmevov.razno
+    }
+]
--- src/app/(general)/odmevi/page.tsx
+++ src/app/(general)/odmevi/page.tsx
@@ -1,8 +1,69 @@
+import { odmevi, TipiOdmevov } from "./data/data"
+import Link from "next/link"
+import { SquareArrowOutUpRight } from "lucide-react"
 
 export default function Odmevi() {
+  const zvezdaOdmevi = odmevi.filter(odmev => odmev.type === TipiOdmevov.zvezda)
+  const veronikaOdmevi = odmevi.filter(odmev => odmev.type === TipiOdmevov.veronika)
+  const arsOdmevi = odmevi.filter(odmev => odmev.type === TipiOdmevov.ars)
+  const raznoOdmevi = odmevi.filter(odmev => odmev.type === TipiOdmevov.razno)
   return (
-    <main>
-        <h1>Odmevi</h1>
+    <main className="px-4 md:px-16 bg-white">
+        <h1 className="text-2xl text-black font-semibold">Odmevi</h1>
+        <section className="text-md md:text-lg pt-4 pb-2">
+            <ul className="pb-2">
+                {zvezdaOdmevi.map(odmev => (
+                    <li className="flex py-2 md:py-0">
+                      <div className="text-neutral-900 mr-2">
+                        {odmev.title}
+                      </div>
+                      <Link className="text-blue-500 hover:text-blue-700 flex  items-center" href={odmev.src} target="_blank"> <SquareArrowOutUpRight className="h-[18px] aspect-square"/></Link>
+                    </li>
+                ))}
+            </ul>
+        </section>
+
+        <h2 className="text-xl text-neutral-700 font-semibold">Odzivi na roman Kavč učiteljice Veronike</h2>
+        <section className="text-md md:text-lg pt-2 pb-4">
+            <ul className="pb-2">
+                {veronikaOdmevi.map(odmev => (
+                    <li className="flex py-2 md:py-0">
+                      <div className="text-neutral-900 mr-2">
+                        {odmev.title}
+                      </div>
+                      <Link className="text-blue-500 hover:text-blue-700 flex items-center justify-center" href={odmev.src} target="_blank"> <SquareArrowOutUpRight className="h-[18px] aspect-square"/></Link>
+                    </li>
+                ))}
+            </ul>
+        </section>
+
+        <h2 className="text-xl text-neutral-700 font-semibold">Art Litera</h2>
+        <section className="text-md md:text-lg pt-2 pb-4">
+            <ul className="pb-2">
+                {arsOdmevi.map(odmev => (
+                    <li className="flex py-2 md:py-0">
+                      <div className="text-neutral-900 mr-2">
+                        {odmev.title}
+                      </div>
+                      <Link className="text-blue-500 hover:text-blue-700 flex items-center" href={odmev.src} target="_blank"> <SquareArrowOutUpRight className="h-[18px] aspect-square"/></Link>
+                    </li>
+                ))}
+            </ul>
+        </section>
+
+        <h2 className="text-xl text-neutral-700 font-semibold">Razno</h2>
+        <section className="text-md md:text-lg pt-2 pb-4">
+            <ul className="pb-2">
+                {raznoOdmevi.map(odmev => (
+                    <li className="flex py-2 md:py-0">
+                      <div className="text-neutral-900 mr-2">
+                        {odmev.title}
+                      </div>
+                      <Link className="text-blue-500 hover:text-blue-700 flex items-center" href={odmev.src} target="_blank"> <SquareArrowOutUpRight className="h-[18px] aspect-square"/></Link>
+                    </li>
+                ))}
+            </ul>
+        </section>
     </main>
   )
 }
