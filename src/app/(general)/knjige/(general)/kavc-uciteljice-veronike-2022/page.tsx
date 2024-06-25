63d9b0b5a983ea62571bc38b26a2bf6623ae9dfa
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-06-25 21:57:04 +0200

    fixed veronika

--- src/app/(general)/knjige/(general)/kavc-uciteljice-veronike-2022/page.tsx
+++ src/app/(general)/knjige/(general)/kavc-uciteljice-veronike-2022/page.tsx
@@ -1,12 +1,20 @@
 import Info from "../../components/info"
 import { books } from "../../data/books";
+import Image from "next/image";
 
 export default function KavcUciteljiceVeronike2022() {
     const bookName = "Kavč učiteljice Veronike (2022)";
     const book = books.find((book) => book.title === bookName);
     return (
-        <div>
-            {book && <Info book={book} />}
-        </div>
+        <main>
+            <div>
+                {book && <Info book={book} />}
+            </div>
+            <div className="container mx-auto">
+                <h2 className="text-2xl text-semibold text-neutral-700 mt-8 py-4">Litera 2022</h2>
+                <Image src="/Litera/1-2.jpg" className="w-full object-contain rounded-sm shadow-md" alt="Kavč učiteljice Veronike 2022" width={1080} height={1080} />
+            </div>
+        </main>
+
     )
 }
