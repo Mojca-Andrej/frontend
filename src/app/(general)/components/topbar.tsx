809b1aad1f6c1706796d7da26db7e30c5f5e53a0
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-06-26 15:42:20 +0200

    added dropdown

--- src/app/(general)/components/topbar.tsx
+++ src/app/(general)/components/topbar.tsx
@@ -1,8 +1,17 @@
+"use client"
 import Link from "next/link"
+import { useState } from "react";
 
 export default function Topbar() {
+
+
+    const [showDropdown, setShowDropdown] = useState(false);
+    const toggleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    };
+  
     return (
-        <main className="flex md:flex-row flex-col py-8 md:px-16 px-4 bg-white md:text-lg items-center md:justify-between">
+        <main className="flex md:flex-row flex-col py-8 md:px-16 px-2 bg-white md:text-lg items-center md:justify-between">
             <div className="logo py-3 md:py-0">
                 <Link href={"/"}>
                     <img className="h-20 md:h-32 aspect-square object-cover rounded-full hover:border-2 shadow-xl cursor-pointer border-violet-300 transition-all"
@@ -12,12 +21,21 @@ export default function Topbar() {
                 </Link>
             </div>
             <div className="buttons flex space-x-1 sm:space-x-2 md:space-x-4 items-center justify-center text-md sm:text-lg md:text-xl">
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="/knjige">knjige</Link>
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="/nastopi">nastopi</Link>
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="/odmevi">odmevi</Link>
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="/galerija">galerija</Link>
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="/branja">branja</Link>
-                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 hover:font-semibold focus:font-semibold hover-border-bottom" href="#footer">kontakt</Link>
+                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/knjige">knjige</Link>
+                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/nastopi">nastopi</Link>
+                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/odmevi">odmevi</Link>
+                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/galerija">galerija</Link>
+                <div className="dropdown relative" >
+                <button onMouseEnter={toggleDropdown} className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold">branja</button>
+                {showDropdown && (
+                    <div className="dropdown absolute bg-white shadow-md top-full rounded-sm w-[150%] mt-0 md:mt-1 left-0 px-1 text-sm md:text-md">
+                        <Link onClick={toggleDropdown} className="block px-2 py-2 text-neutral-800 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/branja/poezija">poezija</Link>
+                        <Link onClick={toggleDropdown} className="block px-2 py-2 text-neutral-800 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/branja/proza">proza</Link>
+                        <Link onClick={toggleDropdown} className="block px-2 py-2 text-neutral-800 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="/branja/za-otroke">za otroke</Link>
+                    </div>
+                )}
+                </div>
+                <Link className="text-neutral-500 hover:text-violet-400 focus:text-violet-300 focus:font-semibold hover-border-bottom" href="#footer">kontakt</Link>
             </div>
             
             <div>
