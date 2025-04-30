83297bf6a4132b3f428dda2a966caad75568bc02
rokrajher <69865825+rokrajher@users.noreply.github.com>
2025-04-30 15:33:27 +0200

    added objave

--- src/app/(general)/objave/data/data.ts
+++ src/app/(general)/objave/data/data.ts
@@ -1,31 +1,43 @@
-
-export interface Objava{
-    mesto: string;
-    datum: string;
-    naslov: string;
-    vsebina: string;
-    link?:string;
+export interface Objava {
+  naslov: string;
+  mesto: string;
+  vsebina: string;
+  datum: string;
+  link?: string;
 }
 
-
 export const objave: Objava[] = [
-    {
-        mesto: "Mlada sodobnost",
-        datum: "november 2024",
-        naslov: "Male čarovnije",
-        vsebina: "Pesmi za otroke",
-    },
-    {
-        mesto: "Za vzorec besede 1 - zbirka prevodov DSP",
-        datum: "december 2024",
-        naslov: "Forgotten by birds",
-        vsebina: "Prevod pesmi Od ptic pozabljen",
-    }, 
-    {
-        mesto: "Radio ARS - Literarni nokturno",
-        datum: "27 februar 2025",
-        naslov: "Od ptic pozabljen",
-        vsebina: "Interpretacija pesmi iz neobjavljene zbirke pesmi Močvirje pozabe",
-        link: "https://ars.rtvslo.si/podkast/literarni-nokturno/289/175112764"
-    },
-]
+  {
+    naslov: "Male čarovnije",
+    mesto: "Mlada sodobnost",
+    vsebina: "Pesmi za otroke",
+    datum: "november 2024",
+  },
+  {
+    naslov: "Forgotten by birds",
+    mesto: "Za vzorec besede 1 - zbirka prevodov DSP",
+    vsebina: "Prevod pesmi Od ptic pozabljen",
+    datum: "december 2024",
+  },
+  {
+    naslov: "Od ptic pozabljen",
+    mesto: "Radio ARS - Literarni nokturno",
+    vsebina:
+      "Interpretacija pesmi iz neobjavljene zbirke pesmi Močvirje pozabe",
+    datum: "februar 2025",
+    link: "https://ars.rtvslo.si/podkast/literarni-nokturno/289/175112764",
+  },
+
+  {
+    naslov: "Knjižničarka",
+    mesto: "Mentor",
+    vsebina: "Kratka zgodba",
+    datum: "marec 2025",
+  },
+  {
+    naslov: "Močvirje pozabe",
+    mesto: "Poetikon 124 - 125",
+    vsebina: "Pesmi iz še neobjavljene pesniške zbirke",
+    datum: "april 2025",
+  },
+];
